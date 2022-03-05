1ee2cdc8934677b3ae84e0974566a06430c70d66
Ronnick7i <94116431+Ronnick7i@users.noreply.github.com>
2022-03-05 11:22:39 +0000

    made some changes to css

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,9 @@
 <!-- the head section -->
 <head>
-<title>My PHP CRUD App</title>
+<title>My Shoe gallery</title>
 <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <!-- the body section -->
 <body>
-<header><h1>My PHP CRUD app</h1></header>
+<header><h1>My Shoe Gallery</h1></header>
